19fa6c13d8cb0049acdbc65a07ae98f43cd4df07
Gabriela Poncet <gabiponcet@gmail.com>
2020-02-29 22:28:12 -0300

    new app

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+ module.exports = {
+     dialect: 'postgres',
+     host: 'localhost',
+     username: 'root',
+     password: 'matrix666',
+     database: 'testeNave',
+     define: {
+         timestamps: true,
+         underscored: true,
+     },
+ }
